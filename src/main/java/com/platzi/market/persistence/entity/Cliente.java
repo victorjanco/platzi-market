94c0a9bcd428072fd1ec5d9d11db3ad34aa27614
victorjanco007 <victorjanco007@gmail.com>
2023-07-03 13:35:45 -0400

    Mapeo de tablas

---
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -0,0 +1,67 @@
+package com.platzi.market.persistence.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private Double celular;
+    private String direccion;
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Compra> compras;
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Double getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Double celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
