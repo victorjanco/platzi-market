26b430fa0d0d318b1b857aafbbc2283a47715e7c
victorjanco007 <victorjanco007@gmail.com>
2023-07-05 14:26:06 -0400

    uso de Mapping

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -1,13 +1,24 @@
 package com.platzi.market;
 
+import com.platzi.market.persistence.ProductoRepository;
+import com.platzi.market.persistence.entity.Producto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class PlatziMarketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlatziMarketApplication.class, args);
+
+//		ProductoRepository productoRepository= new ProductoRepository();
+//
+//		List<Producto> listadoProductos = productoRepository.getAll();
+//		for(Producto row: listadoProductos) {
+//			System.out.println(row.getNombre());
+//		}
 	}
 
 }
